80fb94f4d4def0fd23dce7470c53d83b27e9cb4a
Super User <abitaleg.2010@gmail.com>
2022-10-14 11:26:10 -0700

    0-positive_or_negative.c

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdilb.h>
+#include <time.h>
+
+/* main: entry point*/
+
+/*description: Complete the source code in order to print whether the number stored in the variable n is positive or negative. */
+/*return: 0 */
+
+
+int main(void)
+        
+{
+
+     int n;
+			
+     srand(time(0));
+  
+     n = rand() - RAND_MAX / 2;
+
+     if(n<0);
+
+     {    
+
+            printf("%d is negetive\n", n);
+
+     }   
+
+     else (n==0);
+
+     { 
+
+            printf("%d is zero\n", n); 
+
+     }
+
+     else if(n>0);
+
+     {
+
+             printf("%d is posetive\n", n);	
+
+
+     }
+     
+     return (0);
+
+}
