5689eb07cf867f588953e199c2a580953d74d7cb
Super User <abitaleg.2010@gmail.com>
2022-10-15 11:06:59 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main-ptint a line of code
+ *
+ * Return:0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
