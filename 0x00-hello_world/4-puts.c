bfbd006afc03464da32bed6ddf59d76693773061
Super User <abitaleg.2010@gmail.com>
2022-10-15 09:59:56 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
-
+/*
+ *main - block
+ * Description: puts file
+ * return 0
+ */
 int main(void)
 	{
 	puts("Programming is like building a multilingual puzzle");
