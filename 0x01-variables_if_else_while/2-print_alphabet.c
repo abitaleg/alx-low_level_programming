21c294445b27bd4fae7e19b6896f9311293677c1
Super User <abitaleg.2010@gmail.com>
2022-10-16 12:08:30 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main -Entry
+ * Return: 0
+*/
+int main(void)
+{
+	char lc;
+		for (lc = 'a'; lc <= 'z'; lc++)
+		{
+			putchar(lc);
+		}
+		putchar('\n');
+		return (0);
+}
+
