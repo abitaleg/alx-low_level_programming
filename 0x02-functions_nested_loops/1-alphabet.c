dbe036c3a0e72ce749dd24e69cfd0593f15b3cd9
Super User <abitaleg.2010@gmail.com>
2022-10-19 13:20:16 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints alphabet, in lowercase
+ * Return: 0
+ */
+void print_alphabet(void);
+{
+	char i;
+
+	for (i = 'a'; i<= 'z'; i++)
+	{
+		_putchar(la);
+	}
+	_putchar('\n');
+}
+
