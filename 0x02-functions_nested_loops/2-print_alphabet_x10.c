e8083eac60e7e6fa00103480a870398cfb6a4fc5
Super User <abitaleg.2010@gmail.com>
2022-10-19 14:03:19 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,24 @@
 #include "main.h"
 
 /**
- * print_alphbet x10 -prints lower case alphabet x10
+ * print_alphbet x10 - it prints lower case alphabet x10
  *
- * Rwturn: alwayes 0
  */
 
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	int i;
 	int x;
+
 	for (x = 0; x < 10; x++)
+
 	{
-		for(i ='a'; i <= 'z'; i++);
+		for (i = 'a'; i <= 'z'; i++)
 		{
 			_putchar(i);
 		}
 		_putchar('\n');
 	}
+
 }
 
