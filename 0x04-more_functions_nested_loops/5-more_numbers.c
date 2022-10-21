d9a09620884abd476e630fdc6a2fd0b87b4537f6
Super User <abitaleg.2010@gmail.com>
2022-10-21 16:38:49 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "ain.h"
+
+/**
+ *more_numbers -> prints more number
+ *
+ */
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 15; b++)
+		{
+			_putchar(y + '0');
+		}
+		_putchar('\n');
+	}
+}
