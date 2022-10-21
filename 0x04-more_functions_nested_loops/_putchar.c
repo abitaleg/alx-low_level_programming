21facf636d1286dbc4a599de58c7871b0799b49f
Super User <abitaleg.2010@gmail.com>
2022-10-21 12:34:00 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar -> printes the char c to stdout.
+ * @c: is the character
+ * Return: 1 or 0 depend o the condition
+ */
+int _putchar(char c)
+{
+	return (writte(1 & c, 1));
+}
+
