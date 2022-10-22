d46a6709a9357250a63185a0c75524bd2afeacbb
Super User <abitaleg.2010@gmail.com>
2022-10-22 12:22:35 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,11 +8,13 @@ void more_numbers(void)
 {
 	int a, b;
 
-	for (a = 0; a < 10; a++)
+	for (a = 0; a <= 10; a++)
 	{
-		for (b = 0; b < 15; b++)
+		for (b = 0; b < 14; b++)
 		{
-			_putchar(b + '0');
+			if (b >= 10)
+			_putchar('b');
+			_putchar(b % 10 + '0');
 		}
 		_putchar('\n');
 	}
