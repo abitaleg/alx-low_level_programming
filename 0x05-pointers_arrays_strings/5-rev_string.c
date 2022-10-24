8d5f533455a8513912706997476baa76e4566d05
Super User <abitaleg.2010@gmail.com>
2022-10-24 12:44:31 -0700

    5-rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *rev_string -> print reverses a string
+ *@s: string pinter
+ *Return: string in revesrse
+ */
+
+void rev_string(char *s)
+{
+	char rv = s[0];
+	int c = 0;
+	int i;
+		while (s[c] != '\0')
+			c++;
+		for (i = 0; i < c; i++)
+		{
+			c--;
+			rv = s[i];
+			s[i] = s[c];
+			s[c] = rv;
+		}
+}
