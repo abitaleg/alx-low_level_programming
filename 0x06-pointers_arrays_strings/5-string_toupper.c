38404433658ca446aa62bd7cb354656ecdb2ba84
Super User <abitaleg.2010@gmail.com>
2022-10-27 14:15:40 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *string_toupper->changes all lower case letters
+ * @str:string to be changed
+ * Return: apointer to string
+ */
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+		while (str[i])
+		{
+			if (str[i] >= '0' && str[i] <= '2')
+				str[i] - = 32;
+			i++;
+		}
+		return (str);
+}
