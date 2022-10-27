c5cacb5ee1a8483886181817f08a57aa886920f6
Super User <abitaleg.2010@gmail.com>
2022-10-27 10:49:04 -0700

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,17 +5,12 @@
  * @src: second parameter
  * Return:acharacter
  */
-
 char *_strcat(char *dest, char *src)
 {
-	int len = 0, i;
-		while (dest[len])
-			len++;
-		for (i = 0; src[i] != '\0'; i++)
-		{
-		dest[len] = src[i];
-		len++ = i;
-		}
-		dest[len] = '\0';
+	int index = 0, dest_len = 0;
+		while (dest[index++])
+			dest_len++;
+		for (index = 0; src[index]; index++)
+			dest[dest_len] = src[index];
 		return (dest);
 }
