fdda5c72418d2f2ceecc552e995c87e385cfe83f
Super User <abitaleg.2010@gmail.com>
2022-10-27 15:20:47 -0700

    5-string_toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,8 +11,8 @@ char *string_toupper(char *str)
 	int i = 0;
 		while (str[i])
 		{
-			if (str[i] >= '0' && str[i] <= '2')
-				str[i] -= 32;
+			if (str[i] >= 97 && str[i] <= 122)
+				str[i] = str[i] - 32;
 			i++;
 		}
 		return (str);
