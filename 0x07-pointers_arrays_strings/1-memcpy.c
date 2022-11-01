fb702c39ef7bcd1ec99c3083658f399328f70ed6
Super User <abitaleg.2010@gmail.com>
2022-11-01 10:52:16 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _memcpy - copy memory
+ * @dest:destination memory
+ * @src:sorce memory
+ * @n:interger
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a;
+		for (a = 0; a < n; a++)
+		dest[a] = src[a];
+		return (dest);
+}
