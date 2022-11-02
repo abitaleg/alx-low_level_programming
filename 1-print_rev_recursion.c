51421b2125b17be8efd1db9c4dad5d450ac04fe9
Super User <abitaleg.2010@gmail.com>
2022-11-02 12:57:34 -0700

    1-print_rev_recursion.c

---
+++ 1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion _> print string in reverse
+ * @s: string s
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	s++;
+	_print_rev_recursion(s);
+	s__;
+	_putchar(*s);
+}
