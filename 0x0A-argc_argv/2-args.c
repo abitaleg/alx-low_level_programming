bbc4616086ecbae97acd82121d8ff17323e278c9
Super User <abitaleg.2010@gmail.com>
2022-11-04 11:18:44 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,7 +7,7 @@
  *@argv:arguments
  *Return: Alwayes 0 (success)
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 	for  (i = 0; i < argc; i++)
